bd30fc24ee6d9fcaca1b7726b1350f33df676267
Cody Zacharias <22013186+haccer@users.noreply.github.com>
2024-03-03 20:53:59 -0500

    Add divider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,11 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className={'mb-auto flex flex-col items-center p-24 text-white font-bebasNeue'}>
+    <div className={'mb-auto flex flex-col items-center p-24 divide-slate-500 divide-y divide-double text-white font-bebasNeue'}>
       <div className={'flex flex-col items-center'}>
         <h1 className={'mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl lg:text-6xl'}>
           X E N
         </h1>
-        <hr className={'w-48 h-1 mx-auto my-4 bg-black border-0 rounded md:my-10'} />
       </div>
       <div className={'flex flex-col text-3xl items-center'}>
         <Link href={'https://instagram.com/xen'}>INSTAGRAM</Link>
