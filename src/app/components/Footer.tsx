b64b8afcbe79483c028bd542c3b757d1a2714afc
Cody Zacharias <22013186+haccer@users.noreply.github.com>
2024-03-03 20:56:04 -0500

    See if this fixes footer on mobile

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -10,7 +10,7 @@ function Footer() {
     ]
 
     return (
-        <footer className={'relative flex overflow-x-hidden whitespace-nowrap bg-black'}>
+        <footer className={'fixed inset-x-0 bottom-0 relative flex overflow-x-hidden whitespace-nowrap bg-black'}>
             <div className={'py-2 animate-marquee'}>
                 {links.map((link) => (
                     <SiteLink key={link} href={link} />
